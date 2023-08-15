d62150199f8e837130c1bf996caa667af9e025ab
AbrarZahid050 <abrar.zahid050@gmail.com>
2023-08-15 12:49:00 +0500

    errors removed

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -2,7 +2,7 @@ import { useTheme } from "@mui/material";
 import { ResponsiveChoropleth } from "@nivo/geo";
 import { geoFeatures } from "../data/mockGeoFeatures";
 import { tokens } from "../theme";
-import { mockGeographyData as data } from "../data/mockData";
+import { mockGeographyData as data } from "../data/mockdata";
 
 const GeographyChart = ({ isDashboard = false }) => {
   const theme = useTheme();
